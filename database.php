25592e782bcb216680e3c3252a26b8231446331a
cvuser0 <cvuser0@users.noreply.github.com>
2016-02-06 03:21:38 +0100

    Update database.php

--- database.php
+++ database.php
@@ -1,6 +1,6 @@
 <?php 
-$connection = mysqli_connect("localhost","root","","store-project");
-if($connection == 0){
-    echo "Connection Failed";
-}
+$connection = mysqli_connect("127.0.0.1","root","","store-project"); // there is no difference between localhost and 127.0.0.1, but it should be slightly faster to not consult the hosts file of your server every time you connect.
+//if($connection){ an empty-ish variable evaluates to false so ==0 is not needed, also this entire if-block is not needed because you're not catching the error in it.
+//    echo "Connection Failed";
+//}
 ?>
